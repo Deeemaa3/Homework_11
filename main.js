abde6a70784d7f35d3012c310b8382989e656446
Deeemaa3 <dimaden1@icloud.com>
2024-11-02 22:20:35 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,82 @@
+// Task 1
+
+var userName = 'Дмитрий';
+console.log(userName);
+
+console.log (typeof userName);
+
+var age = 26; 
+console.log(age);
+
+console.log (typeof age);
+
+var single = false; 
+var married = true; 
+if (married>single) {
+    console.log('Женат')
+} else {
+    console.log('Холост')
+}
+
+console.log (typeof single);
+
+var cParent = false; 
+var childless = true; 
+if (childless>cParent) {
+    console.log('Бездетный')
+} else {
+    console.log('Родитель')
+} 
+
+console.log (typeof childless);
+
+
+// Task 2
+
+var height = 23; 
+var width = 10; 
+var s = height*width; 
+
+console.log(s)
+
+// Task 3
+
+var height = 10; 
+var baseArea = 4; 
+var volume = height*baseArea;
+
+console.log(volume)
+
+// Task 4
+
+var a = Infinity - "1"; //Infinity
+
+console.log(a)
+
+var b ="42" + 42; //Nan
+
+console.log(b)
+
+var c = 2 + "1 1"; // 211
+
+console.log(c)
+
+var d = 99 + 101; //200
+
+console.log(d) 
+
+var e = "1" - "1"; //0
+
+console.log(e)
+
+var f = "Result: " + 10/2; //Nan
+
+console.log(f)
+
+var g = 3 + " bananas " + 2 + " apples "; //3 bananas 2 apples 
+
+console.log(g)
+
+
+
+
